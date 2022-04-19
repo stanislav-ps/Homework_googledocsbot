cadcb21a6fa386fdf107260ec4a84d73b2d43d48
Stanislav <stanislav.porin@gmail.com>
2022-04-19 12:10:33 +0300

    Heroku initial

---
+++ settings.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+CREDENTIALS = {
+  "type": "service_account",
+  "project_id": "my-first-project-346709",
+  "private_key_id": os.getenv('PRIVATE_KEY_ID', ''),
+  "private_key": os.getenv('PRIVATE_KEY', ''),
+  "client_email": "omnomnom@my-first-project-346709.iam.gserviceaccount.com",
+  "client_id": os.getenv('CLIENT_ID', ''),
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/omnomnom%40my-first-project-346709.iam.gserviceaccount.com"
+}
+
+TG_TOKEN = os.getenv('TG_TOKEN', '')
+SHEET_ID = '1cPLjCjuGAAmqBychhme-ziV0b0nwvI_vt-erTUQuYEM'
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/1cPLjCjuGAAmqBychhme-ziV0b0nwvI_vt-erTUQuYEM/edit#gid=0'
